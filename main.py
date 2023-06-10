d8d22cb1c21130744e7b1c0448e3767e6515f041
Rohan Gill <136179547+Idkfrr92@users.noreply.github.com>
2023-06-10 20:27:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,97 @@
+import os
+
+ayo = print('''
+
+ ____  ____  _     ____  _     _     ____  _____  ____  ____ 
+
+/   _\/  _ \/ \   /   _\/ \ /\/ \   /  _ \/__ __\/  _ \/  __\
+
+|  /  | / \|| |   |  /  | | ||| |   | / \|  / \  | / \||  \/|
+
+|  \_ | |-||| |_/\|  \_ | \_/|| |_/\| |-||  | |  | \_/||    /
+
+\____/\_/ \|\____/\____/\____/\____/\_/ \|  \_/  \____/\_/\_\
+
+                                        
+
+           MADE BY ZADOW
+
+        ''')
+
+while True:
+
+    dumbmflazy = ('\nDo you wish to know what signs are used for multiplication etc? For Yes type Y For No type N: ')
+
+    dumbmfnigger = dumbmflazy.title()
+
+    dumbmf = input(dumbmfnigger)
+
+    
+
+    if dumbmf == 'Y' or dumbmf == 'y' or dumbmf == 'yes' or dumbmf == 'Yes':
+
+     print('''
+
+   1. * = multiplication.
+
+   2. + = additon.
+
+   3. - = subtraction.
+
+   4. % = remander.
+
+   5. / = division with integers.
+
+   6. // = division without integers.
+
+   7. ** = if you enter 5**3\n   it will be  solved as 5³.''')
+
+    else:
+
+      print('\nAlright Sussy')
+
+    choicelazy = ('\nFor Automation type A for Equation type E: ') 
+
+    lazynigga = choicelazy.title()
+
+    choice = input(lazynigga)
+
+    if choice == 'A' or choice == 'a' or choice == 'automation' or choice == 'Automation':
+
+     x=input('\nEnter The First Number: ')
+
+     y=input('\nEnter The Second Number: ')
+
+     z=input('\nEnter the Symbol: ')
+
+     xyz = (x+z+y)
+
+     result = eval(xyz)
+
+     print('\nProcessing Completed =', result)
+
+    
+
+    elif choice == 'E' or choice == 'e' or choice == 'Equation' or choice == 'equation':
+
+     e = input('\nEnter Your Equation: ')
+
+     result = eval(e)
+
+     print('\nProcessing Completed =', result)
+
+    else:
+
+      pass
+
+    choice3 = input('\nDo you wish to do more calculations? ')
+
+    
+
+    if choice3 == 'Y' or choice3 == 'y' or choice3 == 'yes' or choice3 == 'Yes':
+
+     continue
+
+    else:
+
+      break
